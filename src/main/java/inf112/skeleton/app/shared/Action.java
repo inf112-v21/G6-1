68ee86dbf3f1e36590ae5be8e5e987d2a1ee00c5
num014 <Vilde.Haugstad@student.uib.no>
2021-02-07 19:17:13 +0100

    Implemented enum representing the possible actions

---
+++ src/main/java/inf112/skeleton/app/shared/Action.java
@@ -0,0 +1,11 @@
+package inf112.skeleton.app.shared;
+
+public enum Action {
+    MOVE,
+    ROTATE_LEFT,
+    ROTATE_RIGHT,
+    U_TURN,
+
+    //TODO should MVP actions just be move north, south, west, east
+    //  or should we implement it with rotations?
+}
