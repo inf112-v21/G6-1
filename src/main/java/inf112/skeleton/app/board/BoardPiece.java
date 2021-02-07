307e74db1e1741a41e506ac52dc01b5a972b38b7
num014 <Vilde.Haugstad@student.uib.no>
2021-02-07 20:26:52 +0100

    Added support for board pieces without direction.

--- src/main/java/inf112/skeleton/app/board/BoardPiece.java
+++ src/main/java/inf112/skeleton/app/board/BoardPiece.java
@@ -7,6 +7,20 @@ public class BoardPiece implements IBoardPiece{
     private final TileType tileType;
     private final Direction direction;
 
+    /**
+     * Create a new board piece of a tile type facing north
+     * @param tileType the tile type of the board piece
+     */
+    public BoardPiece(TileType tileType){
+        this.tileType = tileType;
+        this.direction = Direction.NORTH;
+    }
+
+    /**
+     * Create a new board piece of a tile type with a direction
+     * @param tileType the tile type of the board piece
+     * @param direction the direction of the board piece
+     */
     public BoardPiece(TileType tileType, Direction direction){
         this.tileType = tileType;
         this.direction = direction;
