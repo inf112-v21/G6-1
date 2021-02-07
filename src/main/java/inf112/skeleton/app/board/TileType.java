af3f3d692d9caef694fda8015b815d01389e345c
num014 <Vilde.Haugstad@student.uib.no>
2021-02-07 17:11:22 +0100

    Added enums for tile type and directions

---
+++ src/main/java/inf112/skeleton/app/board/TileType.java
@@ -0,0 +1,7 @@
+package inf112.skeleton.app.board;
+
+public enum TileType {
+    FLAG,
+    NORMAL,
+    START_POINT,
+}
---
+++ src/main/java/inf112/skeleton/app/shared/Direction.java
@@ -0,0 +1,8 @@
+package inf112.skeleton.app.shared;
+
+public enum Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST,
+}
