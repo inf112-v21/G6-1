8cd281c586fd674e416495770aec02f85ff46897
thomasRimmereide <thomas.rimme@hotmail.com>
2021-02-08 20:36:59 +0100

    Added interface, class for board logic and empty methods

--- src/main/java/inf112/skeleton/app/boardLogic/BoardLogic.java
+++ src/main/java/inf112/skeleton/app/boardLogic/BoardLogic.java
@@ -1,4 +1,31 @@
 package inf112.skeleton.app.boardLogic;
 
-public class BoardLogic {
+public class BoardLogic implements IBoardLogic {
+
+
+    @Override
+    public Location getLocation() {
+        return null;
+    }
+
+    @Override
+    public Location setNewLocation() {
+        return null;
+    }
+
+    @Override
+    public Location isLocationEmpty() {
+        return null;
+    }
+
+    @Override
+    public Location getXCoordinate() {
+        return null;
+    }
+
+    @Override
+    public Location getYCoordinate() {
+        return null;
+    
+    }
 }
--- src/main/java/inf112/skeleton/app/boardLogic/IBoardLogic.java
+++ src/main/java/inf112/skeleton/app/boardLogic/IBoardLogic.java
@@ -1,4 +1,48 @@
 package inf112.skeleton.app.boardLogic;
 
 public interface IBoardLogic {
+
+    /**
+     * Finds location
+     *
+     * @param
+     * @return
+     */
+    Location getLocation();
+
+
+    /**
+     * Sets new location
+     *
+     * @param
+     * @return
+     */
+    Location setNewLocation();
+
+    /**
+     * Checks if there is an object at a given location...
+     *
+     * @param
+     * @return
+     */
+
+    Location isLocationEmpty();
+
+    /**
+     * Gets the current X coordinate
+     *
+     * @param
+     * @return
+     */
+
+    Location getXCoordinate();
+
+    /**
+     * Gets the current Y coordinate
+     *
+     * @param
+     * @return
+     */
+
+    Location getYCoordinate();
 }
