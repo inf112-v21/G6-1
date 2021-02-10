a511bfd48d77a9b03b81946957ca7d74fd306df4
num014 <Vilde.Haugstad@student.uib.no>
2021-02-10 20:16:37 +0100

    Added new constructor for Player

--- src/main/java/inf112/skeleton/app/player/Player.java
+++ src/main/java/inf112/skeleton/app/player/Player.java
@@ -1,11 +1,12 @@
 package inf112.skeleton.app.player;
 
-import inf112.skeleton.app.gameLogic.GameLogic;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import inf112.skeleton.app.GameLogic.GameLogic;
 import inf112.skeleton.app.location.Location;
 import inf112.skeleton.app.shared.Action;
 import inf112.skeleton.app.robot.Robot;
 
-public class Player implements IPlayer{
+public class Player extends Sprite implements IPlayer{
 
 
     private final Robot robot; // private final Piece piece;
@@ -15,17 +16,21 @@ public class Player implements IPlayer{
     private Location location;
     private Location checkPoint;
 
+
     @Override
     public Action[] getActions(GameLogic board) {
         return new Action[0];
     }
 
 
-    public Player(int id, Robot robot) {
+    public Player(Sprite sprite) {
+        super(sprite);
+        /* This is not needed yet
         this.id = id;
         this.robot = robot;
         this.lifeTokens = 3;
         this.damageTokens = 0;
+        */
     }
 
 
