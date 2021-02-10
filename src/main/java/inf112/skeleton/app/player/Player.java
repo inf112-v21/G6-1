4ddbf801a7e344b72a7cf7f79c0d4fdb28ee9f19
num014 <Vilde.Haugstad@student.uib.no>
2021-02-10 20:20:15 +0100

    Removed errors in player

--- src/main/java/inf112/skeleton/app/player/Player.java
+++ src/main/java/inf112/skeleton/app/player/Player.java
@@ -6,16 +6,16 @@ import inf112.skeleton.app.location.Location;
 import inf112.skeleton.app.shared.Action;
 import inf112.skeleton.app.robot.Robot;
 
-public class Player extends Sprite implements IPlayer{
+public class Player extends Sprite implements IPlayer {
 
-
+/*
     private final Robot robot; // private final Piece piece;
     private int lifeTokens;
     private int damageTokens;
     private final int id;
     private Location location;
     private Location checkPoint;
-
+*/
 
     @Override
     public Action[] getActions(GameLogic board) {
@@ -30,7 +30,7 @@ public class Player extends Sprite implements IPlayer{
         this.robot = robot;
         this.lifeTokens = 3;
         this.damageTokens = 0;
-        */
+
     }
 
 
@@ -41,13 +41,11 @@ public class Player extends Sprite implements IPlayer{
     public int getLifeTokens() {
         return lifeTokens;
     }
-
+/*
     /**
      * Return the players location
      * @return the current location;
      */
-    public Location getLocation() {
-        return location;
-    }
 
+    }
 }
