9c763811f7157ed6b2aab257f8552d563f4e5679
num014 <Vilde.Haugstad@student.uib.no>
2021-02-11 19:37:07 +0100

    Implemented a boardLogic test class with an example test

---
+++ src/test/java/gameLogic/GameLogicTest.java
@@ -0,0 +1,12 @@
+package gameLogic;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class GameLogicTest {
+
+    @Test
+    public void testSimple() throws Exception {
+        assertTrue(false);
+    }
+}
