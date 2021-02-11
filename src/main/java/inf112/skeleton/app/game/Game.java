05931aa9a8e3409c420615d03434d8ea04dd0bc1
sharifi98 <hossein.sharifir@gmail.com>
2021-02-11 14:50:59 +0100

    Tried to implement startGame

--- src/main/java/inf112/skeleton/app/game/Game.java
+++ src/main/java/inf112/skeleton/app/game/Game.java
@@ -1,14 +1,20 @@
 package inf112.skeleton.app.game;
+import inf112.skeleton.app.GameLogic.GameLogic;
+import inf112.skeleton.app.graphics.Graphics;
+import inf112.skeleton.app.player.Player;
 
 
 public class Game implements IGame {
-
-    public Game() {
-
-    }
+    GameLogic gameLogic = new GameLogic();
+    Graphics graphics = new Graphics();
 
     @Override
     public void startGame() {
+        graphics.create();
+        gameLogic.setPlayerStartPosition(20,20);
+        gameLogic.setPlayerSize(32,32);
+        graphics.render();
+        graphics.dispose();
 
     }
 
@@ -19,6 +25,15 @@ public class Game implements IGame {
 
     @Override
     public boolean isGameOver() {
+
+        /*
+        if (flagIsVisited) {
+            return true;
+        } else {
+            return false;
+        }
+
+         */
         return false;
     }
 }
