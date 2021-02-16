4c7bfd0e2d1f05543d6bbdf7221ba6c1366bd544
erlend223 <erl.end@hotmail.com>
2021-02-16 17:15:47 +0100

    Testpush to thomas branch

--- src/main/java/inf112/skeleton/app/cards/Cards.java
+++ src/main/java/inf112/skeleton/app/cards/Cards.java
@@ -2,4 +2,7 @@ package inf112.skeleton.app.cards;
 
 public class Cards implements ICards{
     //TODO Er mulig vi trenger abstrakt klasse
+    public void testPush(){
+        // test commit branch
+    }
 }
