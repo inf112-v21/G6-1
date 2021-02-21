72d8151222169a84e0d28ca0fff5cfea87023145
erlend223 <erl.end@hotmail.com>
2021-02-21 21:42:09 +0100

    created CardDeal class

---
+++ src/main/java/inf112/skeleton/app/cards/CardDeal.java
@@ -0,0 +1,20 @@
+package inf112.skeleton.app.cards;
+
+import java.util.ArrayList;
+import java.util.Random;
+// TODO huske hvordan man reffererer til super/subklasser
+public class CardDeal {
+    private Random rand = new Random();
+    private ArrayList<CardDeck> cardDeck = new ArrayList<CardDeck>();
+    private ArrayList<CardDeck> dealtCards;
+
+    public int random(){
+        return rand.nextInt(84);
+
+    }
+    public CardDeal() {
+        for(int i = 0; i < 9; i++){
+            dealtCards.add(cardDeck.get(random()));
+        }
+    }
+}
