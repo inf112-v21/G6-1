415b0d9da394b78d782cf87279f14ae10513dc1b
ymo004 <yasmin.mohamoud@student.uib.no>
2021-03-03 08:45:49 +0100

    Screens

--- src/main/java/inf112/skeleton/app/screen/HostGameScreen.java
+++ src/main/java/inf112/skeleton/app/screen/HostGameScreen.java
@@ -1,9 +1,6 @@
 package inf112.skeleton.app.screen;
 
 
-/**
- * HostGameScreen shows the player a menu to host a RoboRally game
- */
 public class HostGameScreen  {
 
 }
--- src/main/java/inf112/skeleton/app/screen/JoinGameScreen.java
+++ src/main/java/inf112/skeleton/app/screen/JoinGameScreen.java
@@ -1,8 +1,5 @@
 package inf112.skeleton.app.screen;
 
 
-/**
- * This screen gives the player the functionality to join a multiplayer game
- */
 public class JoinGameScreen {
 }
