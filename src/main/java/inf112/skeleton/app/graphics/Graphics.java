0e9c81e74d312484e61e8c6b0e9d524573b85ccc
erlend223 <erl.end@hotmail.com>
2021-03-03 09:03:34 +0100

    merge graphics

--- src/main/java/inf112/skeleton/app/graphics/Graphics.java
+++ src/main/java/inf112/skeleton/app/graphics/Graphics.java
@@ -9,8 +9,11 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import inf112.skeleton.app.player.LocalHumanPlayer;
+import inf112.skeleton.app.card.Card;
+import inf112.skeleton.app.card.CardMoveLogic;
+import inf112.skeleton.app.player.HumanPlayer;
 import inf112.skeleton.app.screen.MainMenuScreen;
+import inf112.skeleton.app.shared.Action;
 import inf112.skeleton.app.shared.Direction;
 
 
