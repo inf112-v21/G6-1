ffac5fcf650f167e16579c91f07a655c13589cdb
erlend223 <erl.end@hotmail.com>
2021-03-04 22:59:31 +0100

    created values on color and getter

--- src/main/java/inf112/skeleton/app/shared/Color.java
+++ src/main/java/inf112/skeleton/app/shared/Color.java
@@ -1,9 +1,19 @@
 package inf112.skeleton.app.shared;
 
 public enum Color {
-        GREEN,
-        GREY,
-        ORANGE,
-        PINK,
-        PURPLE;
+        GREEN(0),
+        GREY(1),
+        ORANGE(2),
+        PINK(3),
+        PURPLE(4);
+
+        private float color;
+
+        public float getAction(){
+                return this.color;
+        }
+         Color(float color){
+
+                this.color = color;
+        }
 }
