ede0e2e80299356e3c44aa6c9fc03189b1ebd630
sharifi98 <hossein.sharifir@gmail.com>
2021-03-04 20:02:44 +0100

    started to work on CardsAreSentTest

--- src/test/java/inf112/skeleton/app/networkTest/CardsAreSentTest.java
+++ src/test/java/inf112/skeleton/app/networkTest/CardsAreSentTest.java
@@ -1,13 +1,34 @@
 package inf112.skeleton.app.networkTest;
 
+import inf112.skeleton.app.game.Game;
+import inf112.skeleton.app.networking.GameClient;
+import inf112.skeleton.app.networking.GameServer;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.mockito.Mockito.mock;
+
 
 class CardsAreSentTest {
 
+    private static GameServer serverTest;
+    private static GameClient clientTest;
+
+    private static Game gameTest;
+    private static GameClient testClient;
+
     @BeforeEach
-    void setUp() {
+    public static void ServerSetUp() {
+        gameTest = new Game();
+        serverTest = new GameServer("RiskyExchange.tmx");
+        serverTest.run();
+        clientTest = new GameClient(serverTest.getAddress(), gameTest);
+    }
+
+    @Test
+    public void serverGetsCardsTest() {
+
     }
 }
