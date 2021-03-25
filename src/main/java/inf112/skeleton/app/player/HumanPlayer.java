bb6aeef49fe25c73ed17e81af309d43c537af0e3
erlend223 <erl.end@hotmail.com>
2021-03-25 16:01:51 +0100

    merging into master

--- src/main/java/inf112/skeleton/app/player/HumanPlayer.java
+++ src/main/java/inf112/skeleton/app/player/HumanPlayer.java
@@ -123,7 +123,6 @@ public class HumanPlayer extends Player implements InputProcessor {
     public boolean hasPlayerVisitedAllFlags(TiledMapTileLayer flagLayer) {
         TiledMapTileLayer.Cell flagTile = flagLayer.getCell(normalizedCoordinates(playerCurrentXPosition),
                                       normalizedCoordinates(playerCurrentYPosition));
-
         if(flagTile!=null && !this.flagsToVisit.isEmpty() && this.flagsToVisit.get(0) == flagTile.getTile().getId()){
             this.flagsToVisit.remove(0);
         }
@@ -196,23 +195,16 @@ public class HumanPlayer extends Player implements InputProcessor {
 
     //TODO move this to game and improve
 
-    public void singlePlayerRound(ArrayList<Player> players,
-                                  TiledMapTileLayer laserLayer,
-                                  TiledMapTileLayer blueConveyorLayer,
-                                  TiledMapTileLayer yellowConveyorLayer,
-                                  TiledMapTileLayer redGear,
-                                  TiledMapTileLayer greenGear)
-                                  {
+    public void singlePlayerRound(ArrayList<Player> players,TiledMapTileLayer laserLayer,
+                                  TiledMapTileLayer blueConveyorLayer, TiledMapTileLayer yellowConveyorLayer,
+                                  TiledMapTileLayer redGear, TiledMapTileLayer greenGear) {
         if (this.ready) {
             for(int round = 0; round < 5; round ++) {
                 updatePlayerLocation(chosenCards.get(round));
             }
-            //conveyor.findAndRunConveyor(players, yellowConveyorLayer, blueConveyorLayer);
             conveyor.runConveyor(players,yellowConveyorLayer,blueConveyorLayer);
             gear.runGears(players,redGear,greenGear);
             laser.fireAllLasers(players,laserLayer);
-            //boardElements.getBoardElementPositions(players,laserLayer,redGear,greenGear,yellowConveyorLayer,blueConveyorLayer);
-            //boardElements.locatePlayers(players);
             this.movedCards = new ArrayList<>();
             this.chosenCards = new ArrayList<>();
             this.playerDeck = new ArrayList<>();
@@ -289,6 +281,7 @@ public class HumanPlayer extends Player implements InputProcessor {
     public boolean scrolled(int i) {return false;}
     @Override
     public boolean keyDown(int keyPressed) {return false;}
+    //TODO remove before delivery
     @Override
     public boolean keyUp(int keyPressed) {
         if(keyPressed == Input.Keys.UP) updatePlayerYPosition(getPlayerYPosition()+300);
