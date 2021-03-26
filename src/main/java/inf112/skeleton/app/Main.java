1eb812af706ce380fa065824e98bdd95a576d9fd
Vilde <vilde.haugstad@student.uib.no>
2021-03-26 15:26:06 +0100

    Deleted unused classes

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -1,18 +1,8 @@
 package inf112.skeleton.app;
 
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.esotericsoftware.kryonet.Client;
-import com.esotericsoftware.kryonet.Connection;
-import com.esotericsoftware.kryonet.Listener;
-import com.esotericsoftware.kryonet.Server;
 import inf112.skeleton.app.game.Game;
-import inf112.skeleton.app.graphics.Graphics;
-import inf112.skeleton.app.graphics.ScreenHandler;
-
-import javax.swing.*;
-import java.io.IOException;
 
 public class Main {
 
