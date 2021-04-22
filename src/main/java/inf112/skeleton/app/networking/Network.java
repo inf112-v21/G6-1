cbb3b60ba66b71c3b5cc439ca7ad1f42297a24a9
erlend223 <erl.end@hotmail.com>
2021-04-22 10:19:52 +0200

    commit before merging to master

--- src/main/java/inf112/skeleton/app/networking/Network.java
+++ src/main/java/inf112/skeleton/app/networking/Network.java
@@ -23,7 +23,7 @@ public class Network {
         kryo.register(Packets.SendMapNameToPlayer.class);
         kryo.register(Packets.playerInfo.class);
         kryo.register(Packets.SendAction.class);
-
+        kryo.register(inf112.skeleton.app.shared.Action.class);
         kryo.register(java.util.HashMap.class);
         kryo.register(java.util.ArrayList.class);
         kryo.register(int[].class);
